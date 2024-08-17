4cbcca554665a7d09fe6a97847adfba5be2ced73
Engr Ugwu Friday B <fugwungene@gmail.com>
2024-08-17 13:30:38 +0100

    Add more files.

---
+++ create_appoms_db.sql
@@ -0,0 +1,7 @@
+-- A cript that creates a database if not already existing
+
+CREATE DATABASE IF NOT EXISTS appoms_db;
+USE appoms_db;
+CREATE USER 'ugwu'@'localhost' IDENTIFIED BY 'Friday_123';
+GRANT ALL PRIVILEGES ON 'appoms_db' TO 'appoms_dev'@'localhost';
+
